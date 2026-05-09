f25a170cac5eeeea1922d359c22826ec3530b182
David Santamaría Rogado <howl.nsp@gmail.com>
2026-05-09 11:34:09 +0200

    add generic url query function
    Signed-off-by: David Santamaría Rogado <howl.nsp@gmail.com>

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 //RESUELVE TUS EJERCICIOS AQUI
+/* Creo una función genérica de consulta a una URL. */
+const consulta = async (url) => {
+  try {
+    const respuesta = await fetch(url);
+    if (!respuesta.ok) throw respuesta.status;
+    return await respuesta.json();
+  } catch (error) {
+    throw `La consulta produjo un error: ${error}`;
+  }
+}
+
+const getAllBreeds = async () => {
+  try {
+    const datos = await consulta('https://dog.ceo/api/breeds/list/all');
+    if (datos.status !== 'success') throw 'Error al extraer los datos.'
+    return Object.keys(datos.message);
+  }
+  catch (error) {
+    console.log(error);
+  }
+};
