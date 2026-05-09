howl/5-bikram-async1

commit 784bb8185fb182d65f678b3469df93acb50ad921

add exercise 9
Signed-off-by: David Santamaría Rogado <howl.nsp@gmail.com>

index.js
@@ -130,3 +130,22 @@ document.addEventListener('DOMContentLoaded', (ev) => {
     console.log(profile);
   });
 });
+
+/* Ejercicio 9 */
+const getGitHubUserProfileOnlyNameAndUrl = async username => {
+  try {
+    const userProfile = await getGitHubUserProfile(username);
+    if (!userProfile.name || !userProfile.html_url) throw `Fallo al recuperar el perfil del usuario ${username}`;
+    return { name: userProfile.name, html_url: userProfile.html_url };
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const fetchGithubUsers = async usernames => {
+  try {
+    return Promise.all(usernames.map(username => getGitHubUserProfileOnlyNameAndUrl(username)));
+  } catch (error) {
+    console.log(error);
+  }
+};

commit 912f92924c19c6753d8361febb7434d5c56f5060

fix exercise 9: add console logs when Promise.all completes
fixes: 784bb8185fb182d65f678b3469df93acb50ad921

Signed-off-by: David Santamaría Rogado <howl.nsp@gmail.com>

index.js
@@ -144,7 +144,9 @@ const getGitHubUserProfileOnlyNameAndUrl = async username => {
 
 const fetchGithubUsers = async usernames => {
   try {
-    return Promise.all(usernames.map(username => getGitHubUserProfileOnlyNameAndUrl(username)));
+    const promesas = Promise.all(usernames.map(username => getGitHubUserProfileOnlyNameAndUrl(username)));
+    (await promesas).forEach(usuario => console.log(`Name: ${usuario.name} , URL: ${usuario.html_url}`));
+    return promesas;
   } catch (error) {
     console.log(error);
   }